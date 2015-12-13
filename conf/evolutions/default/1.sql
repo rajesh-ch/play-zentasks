6e3a6d6c89f3aaa52371369b6b0b705b6c9670ff
Rajesh Chittineni <checkitraj@gmail.com>
2015-12-12 19:37:52 -0800

    renamed evolutions - user to memeber

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,13 @@
 
 # --- !Ups
 
+create table member (
+  email                     varchar(255) not null,
+  name                      varchar(255),
+  password                  varchar(255),
+  constraint pk_member primary key (email))
+;
+
 create table project (
   id                        bigint not null,
   name                      varchar(255),
@@ -21,53 +28,42 @@ create table task (
   constraint pk_task primary key (id))
 ;
 
-create table member (
-  email                     varchar(255) not null,
-  name                      varchar(255),
-  password                  varchar(255),
-  constraint pk_member primary key (email))
-;
-
 
 create table project_member (
   project_id                     bigint not null,
-  member_email                     varchar(255) not null,
+  member_email                   varchar(255) not null,
   constraint pk_project_member primary key (project_id, member_email))
 ;
+create sequence member_seq;
+
 create sequence project_seq;
 
 create sequence task_seq;
 
-create sequence member_seq;
-
-alter table task add constraint fk_task_assignedTo_1 foreign key (assigned_to_email) references member (email) on delete restrict on update restrict;
+alter table task add constraint fk_task_assignedTo_1 foreign key (assigned_to_email) references member (email);
 create index ix_task_assignedTo_1 on task (assigned_to_email);
-alter table task add constraint fk_task_project_2 foreign key (project_id) references project (id) on delete restrict on update restrict;
+alter table task add constraint fk_task_project_2 foreign key (project_id) references project (id);
 create index ix_task_project_2 on task (project_id);
 
 
 
-alter table project_member add constraint fk_project_member_project_01 foreign key (project_id) references project (id) on delete restrict on update restrict;
+alter table project_member add constraint fk_project_member_project_01 foreign key (project_id) references project (id);
 
-alter table project_member add constraint fk_project_member_member_02 foreign key (member_email) references member (email) on delete restrict on update restrict;
+alter table project_member add constraint fk_project_member_member_02 foreign key (member_email) references member (email);
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+drop table if exists member cascade;
 
-drop table if exists project;
+drop table if exists project cascade;
 
-drop table if exists project_member;
+drop table if exists project_member cascade;
 
-drop table if exists task;
+drop table if exists task cascade;
 
-drop table if exists member;
-
-SET REFERENTIAL_INTEGRITY TRUE;
+drop sequence if exists member_seq;
 
 drop sequence if exists project_seq;
 
 drop sequence if exists task_seq;
-
-drop sequence if exists member_seq;
 
